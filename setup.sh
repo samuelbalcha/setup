f7d5d9dbdfa293010b8a561f93a6dea390cba0ca
samuelbalcha <samuel.balcha@gmail.com>
2015-06-03 13:22:05 +0300

    added commands

--- setup.sh
+++ setup.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
-# for headless setup. 
+# Simple setup.sh for configuring Ubuntu 14.04 LTS EC2 instance.
+# Update, install (Curl, Node, NPM, Express, Git, NVM, Jshint, Repl, Emacs, Herokutoolbelt, MySql & Dotfiles.
+
+#Update
+sudo apt-get update
+
+#Curl
+sudo apt-get install -y curl
+
+#Node
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+
+#NPM npm:node-package manager
+sudo apt-get install npm
+
+#Express
+npm install -g express
+
+#link express
+npm link express
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
-sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -31,6 +44,10 @@ sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
+#MySQL
+sudo apt-get install mysql-server
+
+
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
@@ -45,4 +62,3 @@ ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
-
